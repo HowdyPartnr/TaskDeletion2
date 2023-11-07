c01ea4c02d530a93ced0a600ec68cd8b4b380af1
Håvard Hestetun <hestetun93@gmail.com>
2023-11-07 15:54:54 +0100

    Initial Commit

---
+++ TaskDeletion2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskDeletion2
+//
+//  Created by Håvard Hestetun on 07/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskDeletion2/TaskDeletion2App.swift
@@ -0,0 +1,17 @@
+//
+//  TaskDeletion2App.swift
+//  TaskDeletion2
+//
+//  Created by Håvard Hestetun on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskDeletion2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
